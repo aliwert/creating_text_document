2259a98f73f0c23a91655d6e83d99c9191d0d81d
aliwert <154356044+aliwert@users.noreply.github.com>
2024-06-08 16:09:10 +0300

    Add project files.

---
+++ Metin_Belgesi/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Metin_Belgesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string filename, filepath;
+        StreamWriter sw;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            filename = textBox2.Text;
+            sw = File.CreateText(filepath + "\\"+filename+".txt");
+            sw.Close();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filepath = folderBrowserDialog1.SelectedPath.ToString();
+                textBox1.Text = filepath;
+            }
+        }
+    }
+}
